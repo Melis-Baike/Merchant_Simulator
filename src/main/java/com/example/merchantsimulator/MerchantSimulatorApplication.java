c589d042cca5132fffb746db2c74f272167ba536
Erkin Imakeev <erkinimakeev7@gmail.com>
2023-04-30 15:55:37 +0600

    Added main class

---
+++ src/main/java/com/example/merchantsimulator/MerchantSimulatorApplication.java
@@ -0,0 +1,12 @@
+package com.example.merchantsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantSimulatorApplication.class, args);
+    }
+}

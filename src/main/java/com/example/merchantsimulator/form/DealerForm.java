f227ecb2c4c4dc79d2e15a39a46485bbd0e88ef9
Erkin Imakeev <erkinimakeev7@gmail.com>
2023-04-30 15:54:54 +0600

    Added form class

---
+++ src/main/java/com/example/merchantsimulator/form/DealerForm.java
@@ -0,0 +1,12 @@
+package com.example.merchantsimulator.form;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DealerForm {
+    private String name;
+}
